5aa0cc5ea86870710bb5b8001706e65e0fbfa902
Daniel Adebayo <danieladeba16@gmail.com>
2025-10-20 02:28:12 +0100

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.email-form').addEventListener('submit', function (e) {
-  e.preventDefault();
-  const email = e.target.querySelector('input').value;
-  alert(`Welcome to Streamflix , ${email}! (404 error not found!!!)`);
-});
